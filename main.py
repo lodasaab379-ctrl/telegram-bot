09deec24336d808790357ca8a19cef8071c375fd
lodasaab379-ctrl <lodasaab379@gmail.com>
2025-08-22 16:11:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
+import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-# === Apna Bot Token yaha daalo ===
-TOKEN = ""8208891679:AAE6j5aVkxE8SsAJyLnM_Uhy823qFSR7SoE
+# Token environment variable se lo (Render me set karna hai)
+TOKEN = os.getenv("8208891679:AAE6j5aVkxE8SsAJyLnM_Uhy823qFSR7SoE")
 
 # Start command function
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello! ✅ Your bot is working.")
+    await update.message.reply_text("✅ Bot is live and working!")
 
 # Main function
 if __name__ == "__main__":
     app = Application.builder().token(TOKEN).build()
-
     app.add_handler(CommandHandler("start", start))
-
     app.run_polling()
